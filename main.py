bf786eb44433e5e77f816966a689cbac19a3d87f
Tharaka <tharakau@gmail.com>
2023-10-21 10:50:28 +0530

    added password protection

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import hmac
+
 import streamlit as st
 import os
 import openai
@@ -5,6 +7,34 @@ import openai
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 st.title('Virtual Doctor')
+
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+
+    if st.session_state.get("password_correct", False):
+        return True
+
+
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()
 
 gender_option = st.selectbox(
     'Gender',
